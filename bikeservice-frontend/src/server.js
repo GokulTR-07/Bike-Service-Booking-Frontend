9ffd850efc309c396eab3bb78421385f16d97faa
GokulTR-07 <gokultr1206@gmail.com>
2024-03-20 19:07:19 +0530

    server updated

--- bikeservice-frontend/src/server.js
+++ bikeservice-frontend/src/server.js
@@ -1,3 +1,3 @@
 // export const server = "https://ecom-shop-topaz.vercel.app/api/v2"
 
-export const server = "http://localhost:8000/api/v2"
+export const server = "https://bike-service-booking-backend.vercel.app/api/v2"
