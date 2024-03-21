f33fa625075af60638a3930cb4bbccb9c3077824
GokulTR-07 <gokultr1206@gmail.com>
2024-03-21 12:37:58 +0530

    final commit

--- bikeservice-frontend/src/components/Shop/DashboardMessages.jsx
+++ bikeservice-frontend/src/components/Shop/DashboardMessages.jsx
@@ -375,6 +375,12 @@ const SellerInbox = ({
                     alt=""
                   />
                 )}
+                {item.images && (
+                  <img
+                    src={`${item.images[url]}`}
+                    className="w-[300px] h-[300px] object-cover rounded-[10px] mr-2"
+                  />
+                )}
                 {item.text !== "" && (
                   <div>
                     <div
@@ -389,12 +395,6 @@ const SellerInbox = ({
                       {format(item.createdAt)}
                     </p>
                   </div>
-                )}
-                {item.images && (
-                  <img
-                    src={`${item.images?.url}`}
-                    className="w-[300px] h-[300px] object-cover rounded-[10px] mr-2"
-                  />
                 )}
               </div>
             );
